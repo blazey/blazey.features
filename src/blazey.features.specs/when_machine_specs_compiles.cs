70dead4d89cdfed88abc213b5f9a5a28e2082dd1
Ed Blackburn <ed@ejb.name>
2012-10-22 21:34:18 +0100

    Added psake

---
+++ src/blazey.features.specs/when_machine_specs_compiles.cs
@@ -0,0 +1,10 @@
+﻿using Machine.Specifications;
+
+namespace blazey.features.specs
+{
+    [Subject(typeof(when_machine_specs_compiles), "If mspec runner is working and test compiles, this test will always pass.")]
+    class when_machine_specs_compiles
+    {
+        private It should_compile = () => true.ShouldBeTrue();
+    }
+}
