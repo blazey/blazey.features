3ee4c86b0da4f7795104710c583f072d8735c6ec
Ed Blackburn <ed@ejb.name>
2012-10-22 22:31:16 +0100

    added failng test - need to figure out sub resolver / solution

--- src/blazey.features.specs/when_machine_specs_compiles.cs
+++ src/blazey.features.specs/when_machine_specs_compiles.cs
@@ -30,6 +30,25 @@ namespace blazey.features.specs
         private static Exception _exception;
     }
 
+    class when_component_is_an_unreleased_feature
+    {
+        private Establish that_windsor_is_configured = () =>
+        {
+            _container = new WindsorContainer();
+            _container.Register(Component.For<IFeature>().ImplementedBy<ReleasedFeature>());
+        };
+
+        private Because windsor_resolves = () => _exception = Catch.Exception(
+            () => _resolvedFeature = _container.Resolve<IFeature>());
+
+        private It should_resolve_as_unreleased = () => _resolvedFeature.ShouldBeOfType<UnreleasedFeature>();
+        private It should_not_throw = () => _exception.ShouldBeNull();
+
+        private static IFeature _resolvedFeature;
+        private static IWindsorContainer _container;
+        private static Exception _exception;
+    }
+
 
     internal class UnreleasedFeature : IFeature
     {
