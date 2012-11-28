db438b8582b8a4856b973bc30ce6ba2bc18e102a
Ed Blackburn <ed@ejb.name>
2012-11-28 22:41:21 +0000

    Resolver can recognise feature specs

--- src/blazey.features.specs/when_machine_specs_compiles.cs
+++ src/blazey.features.specs/when_machine_specs_compiles.cs
@@ -1,18 +1,43 @@
 ﻿using System;
+using Castle.Core;
+using Castle.MicroKernel;
+using Castle.MicroKernel.Context;
 using Castle.MicroKernel.Registration;
 using Castle.Windsor;
 using Machine.Specifications;
 
 namespace blazey.features.specs
 {
-    [Subject(typeof(when_machine_specs_compiles), "If mspec runner is working and test compiles, this test will always pass.")]
-    class when_machine_specs_compiles
+    [Subject(typeof (when_machine_specs_compiles),
+        "If mspec runner is working and test compiles, this test will always pass.")]
+    internal class when_machine_specs_compiles
     {
+        private re_use_experiment re_use = new re_use_experiment();
+
         private It should_compile = () => true.ShouldBeTrue();
     }
 
-    class when_component_is_a_released_feature
+    internal class re_use_experiment
     {
+
+        public static int Number { get; private set; }
+
+        private Establish establish = () =>
+            {
+                Number = 10;
+            };
+
+        private It should_be_10 = () => Number.ShouldEqual(10);
+    }
+
+    internal class when_component_is_a_released_feature
+    {
+        private static IFeature _resolvedFeature;
+        private static IWindsorContainer _container;
+        private static Exception _exception;
+        private It should_not_throw = () => _exception.ShouldBeNull();
+        private It should_resolve_as_released = () => _resolvedFeature.ShouldBeOfType<ReleasedFeature>();
+
         private Establish that_windsor_is_configured = () =>
             {
                 _container = new WindsorContainer();
@@ -20,35 +45,93 @@ namespace blazey.features.specs
             };
 
         private Because windsor_resolves = () => _exception = Catch.Exception(
-            ()=> _resolvedFeature = _container.Resolve<IFeature>());
+            () => _resolvedFeature = _container.Resolve<IFeature>());
+    }
 
-        private It should_resolve_as_released = () => _resolvedFeature.ShouldBeOfType<ReleasedFeature>();
-        private It should_not_throw = () => _exception.ShouldBeNull();
-
+    internal class when_component_is_an_unreleased_feature
+    {
         private static IFeature _resolvedFeature;
         private static IWindsorContainer _container;
         private static Exception _exception;
-    }
 
-    class when_component_is_an_unreleased_feature
-    {
         private Establish that_windsor_is_configured = () =>
-        {
-            _container = new WindsorContainer();
-            _container.Register(Component.For<IFeature>().ImplementedBy<ReleasedFeature>());
-        };
+            {
+                _container = new WindsorContainer();
+                _container.Kernel.Resolver.AddSubResolver(new FeatureResolver(_container.Kernel));
+                _container.Register(AllTypes.FromThisAssembly().BasedOn<IFeature>().WithServiceAllInterfaces());
+                _container.Register(
+                    Component.For<Service>(),
+                                Component.For<IFeature>().ImplementedBy<ReleasedFeature>(),
+                                Component.For<IFeatureSpecification<IFeature>>().ImplementedBy<FeatureSpecification>());
+
+              
+            };
 
         private Because windsor_resolves = () => _exception = Catch.Exception(
-            () => _resolvedFeature = _container.Resolve<IFeature>());
+            () => _resolvedFeature = _container.Resolve<Service>().Feature);
 
-        private It should_resolve_as_unreleased = () => _resolvedFeature.ShouldBeOfType<UnreleasedFeature>();
         private It should_not_throw = () => _exception.ShouldBeNull();
-
-        private static IFeature _resolvedFeature;
-        private static IWindsorContainer _container;
-        private static Exception _exception;
+        private It should_resolve_as_unreleased = () => _resolvedFeature.ShouldBeOfType<UnreleasedFeature>();
     }
 
+    public class Service
+    {
+        public IFeature Feature { get; private set; }
+
+        public Service(IFeature feature)
+        {
+            Feature = feature;
+        }
+    }
+
+    public class FeatureResolver : ISubDependencyResolver
+    {
+        private readonly IKernel _kernel;
+
+        public FeatureResolver(IKernel kernel)
+        {
+            _kernel = kernel;
+
+        }
+
+        public bool CanResolve(CreationContext context, ISubDependencyResolver contextHandlerResolver,
+                               ComponentModel model, DependencyModel dependency)
+        {
+            return true;
+        }
+
+        public object Resolve(CreationContext context, ISubDependencyResolver contextHandlerResolver,
+                              ComponentModel model, DependencyModel dependency)
+        {
+
+            var featurespecificationType = typeof (IFeatureSpecification<>)
+                .GetGenericTypeDefinition()
+                .MakeGenericType(new[] {dependency.TargetItemType});
+
+            var featureSpecification = (IFeatureSpecification<object>) _kernel.Resolve(featurespecificationType);
+
+            return featureSpecification.On() ? _kernel.Resolve(dependency.TargetItemType) : featureSpecification.Off();
+        }
+    }
+
+    internal interface IFeatureSpecification<out TFeature> where TFeature : class
+    {
+        bool On();
+        TFeature Off();
+    }
+
+    internal class FeatureSpecification : IFeatureSpecification<IFeature>
+    {
+        public bool On()
+        {
+            return false;
+        }
+
+        public IFeature Off()
+        {
+            return new UnreleasedFeature();
+        }
+    }
 
     internal class UnreleasedFeature : IFeature
     {
@@ -56,10 +139,9 @@ namespace blazey.features.specs
 
     internal class ReleasedFeature : IFeature
     {
-        
     }
 
-    internal interface IFeature
+    public interface IFeature
     {
     }
 }
