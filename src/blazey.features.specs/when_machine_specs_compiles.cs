e2da679d52ae5bdd32a8ac80634c6fe1c6caba18
Ed Blackburn <ed@ejb.name>
2012-12-02 09:49:24 +0000

    moved files into folders

--- src/blazey.features.specs/when_machine_specs_compiles.cs
+++ src/blazey.features.specs/when_machine_specs_compiles.cs
@@ -6,8 +6,6 @@ namespace blazey.features.specs
         "If mspec runner is working and test compiles, this test will always pass.")]
     internal class when_machine_specs_compiles
     {
-        private re_use_experiment re_use = new re_use_experiment();
-
         private It should_compile = () => true.ShouldBeTrue();
     }
 }
