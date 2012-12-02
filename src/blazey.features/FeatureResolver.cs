df0acd3c3c434f63da11cf156441d9293f90a595
Ed Blackburn <ed@ejb.name>
2012-12-02 18:41:53 +0000

    Added todo notes..plenty still to do.

--- src/blazey.features/FeatureResolver.cs
+++ src/blazey.features/FeatureResolver.cs
@@ -17,13 +17,18 @@ namespace blazey.features
         public bool CanResolve(CreationContext context, ISubDependencyResolver contextHandlerResolver,
                                ComponentModel model, DependencyModel dependency)
         {
+            //TODO: identify if type has corresponding IFeatureSpecification<T>
             return true;
         }
 
         public object Resolve(CreationContext context, ISubDependencyResolver contextHandlerResolver,
                               ComponentModel model, DependencyModel dependency)
         {
-
+            /*
+             * TODO: If type has corrsponding IFeatureSpecification invoke Default on corresponding type, 
+             * if true return Default instance. Otherwise invoke Feature member and resolve instance 
+             * according to behaviour of IFeatureSpecification implementation.
+             */
             var featurespecificationType = typeof (IFeatureSpecification<>)
                 .GetGenericTypeDefinition()
                 .MakeGenericType(new[] {dependency.TargetItemType});
